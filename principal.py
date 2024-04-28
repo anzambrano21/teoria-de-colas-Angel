e3ae365fd9db3ca955f1f60aa013a468f8460a59
anzambrano21 <lolepico1234@gmail.com>
2024-04-27 21:33:41 -0400

    creacion de la pagina

--- principal.py
+++ principal.py
@@ -1,2 +1,13 @@
 import flet
 from flet import Page, Column,Row, IconButton ,Text
+
+class pruevas:
+    def __init__(self,page:Page):
+        self.page=page
+        num_Nodo=0
+def main(page:Page):
+    page.title="Teoria de nodos"
+    pruevas(page)
+if __name__=="__main__":
+
+        flet.app(main)
