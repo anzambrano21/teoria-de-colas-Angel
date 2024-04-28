07c437eb7a225a3e3b7511b52cc689cd739287ef
anzambrano21 <lolepico1234@gmail.com>
2024-04-28 12:20:42 -0400

    creacion de la clase de nodos

---
+++ nodos.py
@@ -0,0 +1,8 @@
+
+#creamos una clase de nodos con un tiempo de llegada y de atender
+class nodo:
+    def __init__(self,evn,Tlleg,Toper,nombre):
+        self.even=evn
+        self.Tlleg=Tlleg
+        self.Toper=Toper
+        self.nombre=nombre
--- principal.py
+++ principal.py
@@ -1,8 +1,11 @@
-import flet
+import flet,random,simpy
 from flet import Page, Column,Row, ElevatedButton ,Text
+from nodos import nodo
+
 
 class pruevas:
-    def __init__(self,page:Page):
+    def __init__(self,env,page:Page):
+        self.env=env
         self.page=page
         self.col=Column(width=500,height=200,scroll=flet.ScrollMode.ALWAYS)
         self.contenido()
@@ -25,10 +28,23 @@ class pruevas:
 
         for i in range(int(self.n_TexNodo.value)):
             
-            self.num_Nodo.append(flet.TextField(label="Nodo N "+str(i)+""))
+            self.num_Nodo.append(flet.TextField(label=f"tiempo de llegada del nodo {i+1}"))
         self.col.controls=self.num_Nodo
         self.page.add(self.col)
         self.page.update()
+    def llegadaC(self,cliente,nodo):
+        print(f"{cliente} llega al nodo {nodo.nombre} en t = {self.env.now}")
+        yield self.env.timeout(nodo.tiempo_llegada)
+        print(f"{cliente} se mueve al nodo B en t = {self.env.now}")
+        yield self.env.timeout(nodo.tiempo_servicio)
+        print(f"{cliente} es atendido en B en t = {self.env.now}")
+    def operarC(self):
+        TIEMPO_CORTE_MIN = 15
+        TIEMPO_CORTE_MAX = 30
+        nodos=[]
+        for i in range():
+             nodos.append(nodos(self.env,self.num_Nodo[i].value,random.uniform(TIEMPO_CORTE_MIN,TIEMPO_CORTE_MAX)))
+         
          
          
 def main(page:Page):
@@ -36,10 +52,12 @@ def main(page:Page):
     page.window_width=500
     
     page.window_resizable=False
-    
+    env = simpy.Environment()
+    # Parámetros
+
     page.title="Teoria de nodos"
-    pruevas(page)
+    pruevas(env,page)
     page.update()
 if __name__=="__main__":
 
-        flet.app(main)
+        flet.app(main())
