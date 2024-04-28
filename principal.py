a8cdabe0a58ddfdfa257440420e2078122f7e81d
anzambrano21 <lolepico1234@gmail.com>
2024-04-28 00:11:58 -0400

    ingreso de N nodos pero no se como reiniciar

--- principal.py
+++ principal.py
@@ -1,13 +1,40 @@
 import flet
-from flet import Page, Column,Row, IconButton ,Text
+from flet import Page, Column,Row, ElevatedButton ,Text
 
 class pruevas:
     def __init__(self,page:Page):
         self.page=page
-        num_Nodo=0
+        self.contenido()
+
+        self.num_Nodo=[]
+    def contenido(self):
+         self.n_TexNodo=flet.TextField(label="Numero de nodos")
+         self.row=Column(controls=[
+            self.n_TexNodo,
+            flet.ElevatedButton(text="generar",on_click=self.generar)
+
+         ])
+         
+         self.page.add(self.row)
+    def generar(self,event):
+        if (len(self.num_Nodo)>0):
+            self
+        for i in range(int(self.n_TexNodo.value)):
+            self.num_Nodo.append(flet.TextField(label="Nodo N "+str(i)+""))
+        self.col=Column(self.num_Nodo)
+        self.page.add(self.col)
+        self.page.update
+         
+         
 def main(page:Page):
+    page.window_height=400
+    page.window_width=500
+    
+    page.window_resizable=False
+    
     page.title="Teoria de nodos"
     pruevas(page)
+    page.update()
 if __name__=="__main__":
 
         flet.app(main)
