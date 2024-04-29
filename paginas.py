2992dc58abc1b1ddf4a0426ba83483bb9f7b50a0
anzambrano21 <lolepico1234@gmail.com>
2024-04-29 10:03:22 -0400

    mejora

--- paginas.py
+++ paginas.py
@@ -27,9 +27,7 @@ class pruevas:
             #self.page.controls.pop(3)
             self.num_Nodo.clear()
 
-        for i in range(int(self.n_TexNodo.value)):
-            
-            self.num_Nodo.append(flet.TextField(label=f"tiempo de llegada del nodo {i+1}"))
+        for i in range(int(self.n_TexNodo.value)): self.num_Nodo.append(flet.TextField(label=f"tiempo de llegada del nodo {i+1}"))
         self.col.controls=self.num_Nodo
         self.page.add(self.col)
         self.page.add(flet.ElevatedButton(text="operar",on_click=self.operarC))
@@ -40,21 +38,16 @@ class pruevas:
         nodos=[]
         Probalilida=[]
         
-        for i in range(len(self.num_Nodo)):
-             nodos.append(Nodo(self.env,int(self.num_Nodo[i].value),(15,30),nombre=f"{i}"))
+        for i in range(len(self.num_Nodo)): nodos.append(Nodo(self.env,int(self.num_Nodo[i].value),(15,30),nombre=f"{i}"))
         
-        for nodo in nodos:
-            self.env.process(nodo.llegada_cliente())
-        
+        for nodo in nodos: self.env.process(nodo.llegada_cliente())
 
         self.env.run(until=100)
 
-        for i in range(len(nodos)):
-             Probalilida.append(nodos[i].Tlleg/nodos[i].Toper[1])
+        for i in range(len(nodos)): Probalilida.append(nodos[i].Tlleg/nodos[i].Toper[1])
         res=Column(width=500,height=200,scroll=flet.ScrollMode.ALWAYS)
         resp=[]     
-        for i in range(len(Probalilida)):
-            resp.append(flet.Text(value="probabilidad de espera es {0}".format(Probalilida[i])))
+        for i in range(len(Probalilida)):resp.append(flet.Text(value="probabilidad de espera es {0}".format(Probalilida[i])))
         res.controls=resp
         self.page.add(res)
         self.page.update()
